29b07a7d0a1589762a5e7f56dba3729069627030
Ben Zaaiman <ben.zaaiman@gmail.com>
2017-05-24 04:17:24 +0000

    Add edit action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -18,6 +18,28 @@ class TodosController < ApplicationController
     @todo = Todo.find(params[:id])
   end
   
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+  
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      flash[:notice] = '@todo was succesfully updated.'
+      redirect_to todo_path(@todo)
+    else
+      render 'edit'
+    end
+  end
+  
+  def index
+    
+  end
+  
+  def destroy
+    
+  end
+  
   private
   
     def todo_params
